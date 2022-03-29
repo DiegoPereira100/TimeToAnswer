15290cd7bbf34a7309cb3173cc1639a0501c97db
diego <diegopgcds10@gmail.com>
2022-03-28 22:25:46 -0300

    using validations on rails

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -12,14 +12,14 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
  private
  
  def params_user
-   params.require(:user).permit(:first_name, :last_name, :email,
-  :password, :password_confirmation)
+ params.require(:user).permit(:first_name, :last_name, :email,
+:password, :password_confirmation)
  end
  def verify_password
-   if params[:user][:password].blank? &&
-  params[:user][:password_confirmation].blank?
-   params[:user].extract!(:password, :password_confirmation)
-  end
-end
+ if params[:user][:password].blank? &&
+params[:user][:password_confirmation].blank?
+ params[:user].extract!(:password, :password_confirmation)
+ end
+ end
 
 end
